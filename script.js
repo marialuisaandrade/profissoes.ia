faafc41737a750c177258c148fce0a07411b3c1b
Maria <140732755+marialuisaandrade@users.noreply.github.com>
2025-06-16 08:46:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,67 +6,66 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Uma IA é capaz diagnosticar doenças com 98% de precisão. O hospital recomenda o uso constante. E agora?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Usar a IA em todos os diagnósticos",
+                afirmacao: "Você acerta muito mais nos diagnóstico, mas perde a prática e começa a depender de IA para tudo."
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Usar a IA apenas em casos extremos",
+                afirmacao: "Mantém sua habilidade médica viva, mas perde diagnósticos precoces que poderiam ter salvado vidas."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-        alternativas: [
+        enunciado: "Robôs cirurgiões agora operam sem as mãos humanas. Um hospital oferece a você o cargo de supervisor de robôs"
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Aceitar liderar a equipe automatizada.",
+                afirmacao: "Você se torna referência em cirurgia automatizada, mas deixa de operar e sente-se distante da prática"
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Continuar operando com sua equipe humana",
+                afirmacao: "Mantém a conexão com a medicina, mas é visto como ultrapassado por hospitais modernos."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "A IA prevê doenças com até 10 anos de antecedência com base em dados genéticos e hábitos",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Revelar todos os riscos aos pacientes",
+                afirmacao: "Muitos pacientes mudam de vida, mas outros entram em pânico por doenças que talvez nunca aconteçam"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Revelar apenas o que o paciente quiser,
+                afirmacao: "Menos estresse nos pacientes, mas vocêperde oportunidades de prevenção."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "A IA oferece planos de tratamento personalizados em segundos para cada paciente",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Confiar nos planos da IA e apenas assinar",
+                afirmacao: "Ganha tempo e atende mais, mas pacientes sentem-se tratados como números"
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Revisar cada plano com cuidado",
+                afirmacao: "Oferece cuidado humanizado,  mas vive sobrecarregado(a)"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "O hospital quer reduzir o número de médicos físicos e adotar avatares de IA para atendimento básico",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Apoiar a mudança e realocar os médicos",
+                afirmacao: "O hospital atende mais rápido e gasta menos, mas os pacientes e médicos reclamam da frieza"
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Defender a presença humana no atendimento",
+                afirmacao: "Atendimento mais empático,  mas o hospital perde eficácia e verba."
             }
         ]
     },
